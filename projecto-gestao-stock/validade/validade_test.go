3b72677e5b4363c5e02afd12be2525810cf65953
trainee <trainee@dev1.lan>
2024-05-07 15:12:03 +0100

    Falha ao criar novo propduto

--- projecto-gestao-stock/validade/validade_test.go
+++ projecto-gestao-stock/validade/validade_test.go
@@ -9,14 +9,32 @@ func TestCriaNovoProduto_ProdutoCriado(t *testing.T) {
 	// Arrange
 	iv := NewInventario()
 
-	produto := Produto{ID: "12ds", Descricao: "Sumol", Categoria: "Bebidas", AmbienteDeArmazenamento: "Refrigerado"}
+	produto := &Produto{ID: "12ds", Descricao: "Sumol", Categoria: "Bebidas", AmbienteDeArmazenamento: "Refrigerado"}
 
 	// Act
-	iv.CriaNovoProduto(&produto)
+	iv.CriaNovoProduto(produto)
 
 	// Assert
 	if iv.Produtos[0].ID != produto.ID {
-		t.Logf("Esperado %v, Recebido %v: ", produto, iv.Produtos)
+		t.Errorf("Esperado %v, Recebido %v: ", produto, iv.Produtos)
 	}
+
+}
+
+
+func TestFalhaAoCriarNovoProduto(t *testing.T) {
+		// Arrange
+		iv := NewInventario()
+
+		produto := &Produto{ID: "12ds", Descricao: "Sumol", Categoria: "Bebidas", AmbienteDeArmazenamento: "Refrigerado"}
+	
+		// Act
+		iv.CriaNovoProduto(produto)
+	
+		// Assert
+		if iv.Produtos[0].ID == produto.ID {
+			t.Errorf("Esperado %v, Recebido %v: ", produto, iv.Produtos)
+		}
+	
 
 }
