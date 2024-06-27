f83f44d5ba9191b9b726f90858fe1253c3e815be
Tito Cazanga <tcazanga59@gmail.com>
2024-06-27 09:09:12 +0100

    consulta

--- centro-veterinario-luanda/cmd/main.go
+++ centro-veterinario-luanda/cmd/main.go
@@ -4,24 +4,23 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
-	"github.com/Tito-Cazanga/tcazanga-estagio-2024-04/application"
 	"github.com/Tito-Cazanga/tcazanga-estagio-2024-04/adapter/inmem"
+	"github.com/Tito-Cazanga/tcazanga-estagio-2024-04/application"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "go-gopher-cli",
-		Short: "Gopher CLI in Go",
-		Long:  `Gopher CLI application written in Go.`,
+		Use:   "cvl",
+		Short: "Aplicação CLI para internar paciente.",
+		Long:  `Aplicação CLI para consulta paciente usando Cobra em Go.`,
 	}
 
-	getCmd := &cobra.Command{
-		Use:   "get",
-		Short: "This command will get the desired Gopher",
-		Long:  `This get command will call GitHub repository in order to return the desired Gopher.`,
-
 	
+	consultarCmd := &cobra.Command{
+		Use:   "consulta_paciente",
+		Short: "Este comando irá consultar paciente",
+		Long:  `Este comando consulta_paciente verifica se o paciente esta internado e retorna se sim ou nao.`,
 
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) < 2 {
@@ -39,11 +38,11 @@ func main() {
 				fmt.Println(err)
 				return
 			}
-			fmt.Println("Paciente internado com sucesso!")
+			fmt.Println("Paciente esta internado internado!")
 		},
 	}
 
-	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(consultarCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
