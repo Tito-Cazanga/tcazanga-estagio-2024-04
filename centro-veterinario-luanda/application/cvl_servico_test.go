a1b9de38e4caf976fd60e9b9a015d62750cb755d
Tito-Cazanga <tcazanga59@gmail.com>
2024-06-28 05:08:42 +0100

    Internar Paciente

--- centro-veterinario-luanda/application/cvl_servico_test.go
+++ centro-veterinario-luanda/application/cvl_servico_test.go
@@ -28,7 +28,7 @@ func TestInternarPaciente(t *testing.T) {
 		}
 		})
 
-		
+
 	t.Run("Deve internar paciente com dados válidos", func(t *testing.T) {
 		internarPacienteComSucesso(t, *servico, "0011", "Max", "Rafeiro")
 	})
@@ -42,8 +42,10 @@ func TestInternarPaciente(t *testing.T) {
 	})
 }
 
-func internarPacienteComSucesso(t *testing.T, servico application.PacienteServico, pacienteId string, pacienteNome string, pacienteRaca string) {
+func internarPacienteComSucesso(t *testing.T, servico application.PacienteServico, 			
+	pacienteId string, pacienteNome string, pacienteRaca string) {
 	err := servico.InternarPaciente(pacienteId, pacienteNome, pacienteRaca)
+	
 	if err != nil {
 		t.Errorf("Erro ao internar paciente: %v", err)
 	}
@@ -51,12 +53,14 @@ func internarPacienteComSucesso(t *testing.T, servico application.PacienteServic
 
 func internarPacienteComErro(t *testing.T, servico application.PacienteServico, pacienteId string, pacienteNome string, pacienteRaca string) {
 	err := servico.InternarPaciente(pacienteId, pacienteNome, pacienteRaca)
+	
 	if err == nil {
 		t.Errorf("Erro esperado ao internar paciente com dados inválidos, mas nenhum erro foi retornado")
 	}
 }
 
-func verificarPacienteInternado(t *testing.T, repo inmem.RepositorioemMemoriaPaciente, pacienteId string, pacienteNome string, pacienteRaca string) {
+func verificarPacienteInternado(t *testing.T, repo inmem.RepositorioemMemoriaPaciente, 
+	pacienteId string, pacienteNome string, pacienteRaca string) {
 	
 	paciente, err := repo.EncontrarID(pacienteId)
 	
